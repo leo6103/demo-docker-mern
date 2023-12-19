b1b5d7e51086aea9de2d2f0bf96b12abfb93eaa8
Phan Khoi Nguyen <khoinguyen6103@gmail.com>
2023-12-19 08:37:30 +0000

    Set up server

--- bezkoder-ui/src/http-common.js
+++ bezkoder-ui/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://103.130.213.117:6868/api',
   headers: {
     "Content-type": "application/json"
   }
